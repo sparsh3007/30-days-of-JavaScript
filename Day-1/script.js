60f527b64c63b17c81f62e60f2cccc58137120a2
Sparsh Raj <sparsh.raj30@gmail.com>
2023-11-06 16:29:45 +0530

    added demo of variables

--- Day-1/script.js
+++ Day-1/script.js
@@ -1,7 +1,10 @@
 console.log("Hello from script file!")
 
-let a=15
-console.log(a)
+// let a=15
+// console.log(a)
 
 let b=14+23
 console.log(b)
+
+let a=3000;
+console.log("I love you", a,"!");
---
+++ Day-1/variables.js
@@ -0,0 +1,6 @@
+var a=10
+console.log(a)
+
+a="of the day"
+var g="Greetings"
+console.log(g,a)
